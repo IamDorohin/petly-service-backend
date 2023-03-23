d2f2da1cc618c0ab25f6388507b830783e0a803c
Fyl Andrii <fil.andrey91@gmail.com>
2023-03-23 12:31:03 +0200

    adds basic swagger settings

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 // transport names of variables to environment of variables
 require("dotenv").config();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 // crete server
 const app = express();
 
@@ -32,6 +35,7 @@ app.use("/api/our_friends", friendsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/notices", noticesRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
