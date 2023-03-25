b3f86dbafc565d29a3dc6b6cd2bf3f5d26d56939
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-03-25 14:40:30 +0200

    try to fix folder path midlewars>midleWars

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
 // create route
 const router = express.Router();
-const { auth, upload, cloudinaryMiddleWar, validation, ctrlWrapper } = require("../../middlewares");
-const { profile: ctrl } = require('../../controllers');
-const { joiPetsSchema } = require('../../models/pets');
+const {
+  auth,
+  upload,
+  cloudinaryMiddleWar,
+  validation,
+  ctrlWrapper,
+} = require("../../middleWares");
+const { profile: ctrl } = require("../../controllers");
+const { joiPetsSchema } = require("../../models/pets");
 
-
-router.post('/', auth, upload.single('petsImageUrl'), validation(joiPetsSchema), cloudinaryMiddleWar, ctrlWrapper(ctrl.addPet));
-router.delete('/:petId', auth, validation(joiPetsSchema), ctrlWrapper(ctrl.removePet));
-router.get('/', auth, validation(joiPetsSchema), ctrlWrapper(ctrl.showYourPets))
+router.post(
+  "/",
+  auth,
+  upload.single("petsImageUrl"),
+  validation(joiPetsSchema),
+  cloudinaryMiddleWar,
+  ctrlWrapper(ctrl.addPet)
+);
+router.delete(
+  "/:petId",
+  auth,
+  validation(joiPetsSchema),
+  ctrlWrapper(ctrl.removePet)
+);
+router.get(
+  "/",
+  auth,
+  validation(joiPetsSchema),
+  ctrlWrapper(ctrl.showYourPets)
+);
 
 module.exports = router;
