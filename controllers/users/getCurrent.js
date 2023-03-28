b4cec225aa4f1c7d5ed474a5017a34db4eb69d92
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-03-28 03:28:02 +0300

    add token to current

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,17 +1,18 @@
 const getCurrent = async (req, res) => {
-    const { name, email, id } = req.user;
-    
-    res.json({
-        status: "success",
-        code: 200, 
-        data: {
-            user: {
-                name,
-                email,
-                id
-            }
-        }
-    })
-}
+  const { name, email, id, token } = req.user;
+
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      user: {
+        name,
+        email,
+        id,
+      },
+    },
+    token,
+  });
+};
 
 module.exports = getCurrent;
